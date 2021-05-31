5447ca979c8924d5ba7d05ddad7ca9bd32c222ba
Sam Varner <snick-a-doo@comcast.net>
2021-05-31 13:59:28 -0400

    Changed the test file writer
    Use getopt for arguments. Write additional strings for new test cases.
    
    Removed u_test_data and U_test_data. Now there's just test_data with 8-bit chars and
    test_data_wide with 16-bit chars.

--- write.cc
+++ write.cc
@@ -1,89 +1,73 @@
+#include <getopt.h>
+
 #include <cstring>
 #include <fstream>
 #include <iostream>
 #include <string>
 
+/// Write the bytes of the value to the stream.
 template <typename T> void write(std::ostream& os, T value)
 {
-    std::cout << "number " << value << std::endl;
     os.write(reinterpret_cast<char*>(&value), sizeof value);
 }
 
-template <typename T> void write_string(std::ostream& os, const T* str, std::size_t length)
+/// Write a char string to the stream.
+template <> void write(std::ostream& os, const char* str)
 {
-    os.write(reinterpret_cast<const char*>(str), (length + 1)*(sizeof str[0]));
-    // for (size_t i = 0; os && str[i] != 0; ++i)
-    //     os.write(reinterpret_cast<const char*>(str + i), sizeof str[i]);
+    os << str << std::ends;
 }
 
-// template<> void write(std::ostream& os, const char* str)
-// {
-//     std::cout << "string8 " << str << std::endl;
-//     write_string(os, str);
-// }
-// template<> void write(std::ostream& os, const char16_t* str)
-// {
-//     std::cout << "string16 " << str << std::endl;
-//     write_string(os, str);
-// }
-// template<> void write(std::ostream& os, const char32_t* str)
-// {
-//     std::cout << "string32 " << str << std::endl;
-//     write_string(os, str);
-// }
+/// Write a wide string to a stream.
+template <> void write(std::ostream& os, const char16_t* str)
+{
+    do
+        os.write(reinterpret_cast<const char*>(str), sizeof(char16_t));
+    while (*str++);
+}
 
 int main(int argc, char** argv)
 {
-    auto usage = "Usage: write [-u|-U] FILE";
-    if (argc < 2)
+    bool wide = false;
+    char c;
+    while ((c = getopt(argc, argv, "u")) != -1)
     {
-        std::cerr << usage;
+        if (c == 'u')
+            wide = true;
+    }
+    if (argc - optind != 1)
+    {
+        std::cerr << "Usage: write [-u] FILE\n";
         exit(-1);
     }
-    std::string file = argv[1];
-    std::size_t char_width = 1;
-    if (argc > 2)
-    {
-        std::string opt = argv[1];
-        if (opt == "-u")
-            char_width = 2;
-        else if (opt == "-U")
-            char_width = 4;
-        else
-        {
-            std::cerr << usage;
-            exit(-1);
-        }
-        file = argv[2];
-    }
-    std::cout << char_width << std::endl;
+    std::string file = argv[optind];
+
     std::ofstream os(file);
     write(os, 1.23); // 3ff3 ae14 7ae1 47ae -> ae47 e17a 14ae f33f
     write(os, 432); // 1b0 -> b001 0000
-    write(os, 0x00ff00ff00ff00ffL);
-    switch (char_width)
+    write(os, 0x00ffeeffeeffeeffL); // ffee... = 72038902055038719
+    if (wide)
     {
-    case 1:
-        write_string(os, "moo", 3); // moo -> 6d6f 6f
-        write_string(os, "weeping willow", 14);
-        break;
-    case 2:
-        write_string(os, u"moo", 3); // moo -> 6d6f 6f
-        write_string(os, u"wæèþîñg wïllöw", 14);
-        break;
-    case 4:
-        write_string(os, U"moo", 3); // moo -> 6d6f 6f
-        write_string(os, U"weping willow", 14);
-        break;
-    default:
-        std::cerr << usage;
-        exit(-1);
-        break;
+        write(os, u"moo"); // moo -> 6d6f 6f, no preceeding zero
+        write(os, u"moo"); // moo -> 6d6f 6f
+        write(os, u"weeping willow");
+        // Offset the next string by 1 byte.
+        char offset = 0;
+        os << offset;
+        write(os, u"first\tsecond\nthird"); // 3 strings
+        write(os, u"third");
+    }
+    else
+    {
+        write(os, "moo"); // moo -> 6d00 6f00 6f00, no preceeding zero
+        write(os, "moo"); // moo -> 6d00 6f00 6f00
+        write(os, "weeping willow");
+        write(os, "first\tsecond\nthird"); // 3 strings
+        write(os, "third");
     }
     write(os, -1); // ffff ffff
     write(os, 0);
     write(os, 0);
     write(os, 0);
-    write(os, 1);
+    write(os, 1); // 0100 0000
     return os.good();
 }
